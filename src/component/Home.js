c5b17cf0643863d294fb05d587758719397223c2
TK <thiratikan@gmail.com>
2023-02-11 16:20:14 -0500

    update welcome page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import videoBG from "../video/background.mov";
+import { Main, Content } from "../styles/HomeStyling";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Main>
+      <video src={videoBG} autoPlay loop muted />
+      <Content>
+        <div className="font">
+          <h1>WELCOME</h1>
+          <h3>TO</h3>
+          <h3>FLYING RABBIT</h3>
+          <h4>Start &#10141;</h4>
+        </div>
+      </Content>
+    </Main>
+  );
 }
 
 export default Home;
---
+++ src/styles/HomeStyling.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const Main = styled.div`
+  height: 100vh;
+  video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    position: fixed;
+  }
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  .font {
+    font-family: "Fredoka One", cursive;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    padding: 10em;
+  }
+  h1 {
+    font-size: 150px;
+  }
+  h3 {
+    font-size: 70px;
+  }
+  h4 {
+    font-family: "Roboto", sans-serif;
+    font-size: 30px;
+    margin-top: 2em;
+    cursor: pointer;
+  }
+`;
